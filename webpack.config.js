18066859431fc9a43e661f0cd183dd6bf505853b
Filip Sotirov <me@philipsotirov.com>
2017-08-25 14:12:20 +0300

    Webpack initial setup 🦄

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+
+const config = {
+  context: __dirname,
+  entry: './src/App.js',
+  devtool: 'cheap-eval-source-map',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/public/'
+  },
+  devServer: {
+    publicPath: '/',
+    contentBase: './public',
+    historyApiFallback: true
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
+
+console.log('NODE_ENV: ' + process.env.NODE_ENV);
+
+if (process.env.NODE_ENV === 'production') {
+  config.devtool = false;
+  config.output.path = path.join(__dirname, 'build');
+  config.output.filename = '[name].bundle.[chunkhash].js';
+}
+
+// TODO: check out https://simonsmith.io/organising-webpack-config-environments/
+
+module.exports = config;
