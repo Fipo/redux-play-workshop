94b89ef44e624966d5d103a1473ec4ca1c552d7d
Filip Sotirov <me@philipsotirov.com>
2017-08-26 05:23:58 +0300

    Example Counter Implementation
    Implementation of the counter example from the redux examples page:
    
    * http://redux.js.org/docs/introduction/Examples.html
    * https://github.com/reactjs/redux/tree/master/examples/counter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
+import Counter from './components/Counter';
 
-const App = () => {
-  return <h1>Hello Webpack!</h1>;
-};
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const el = document.getElementById('root');
 
-render(<App />, document.getElementById('root'));
+const App = () =>
+  <Provider store={store}>
+    <Counter />
+  </Provider>;
+
+render(<App />, el);
