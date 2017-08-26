08b9a55537786b6d83b97ab606008f319036b580
Filip Sotirov <me@philipsotirov.com>
2017-08-26 05:48:16 +0300

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import rootReducer from './reducers';
-import Counter from './components/Counter';
 
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 const el = document.getElementById('root');
 
-const App = () =>
-  <Provider store={store}>
-    <Counter />
-  </Provider>;
+const App = () => <h1>Hello Redux</h1>;
 
 render(<App />, el);
